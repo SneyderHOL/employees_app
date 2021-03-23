5f6bc2a3318abdefe8bce337ce3738552b13146d
SneyderHOL <1507@holbertonschool.com>
2021-03-23 18:12:16 -0500

    Update table and validatations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  enum department: { accounting: "Accounting", finance: "Finance", operations: "Operations", security: "Security", human_resources: "Human Resources" }, _prefix: :department
+  validates :first_name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :last_name, presence: true, length: { minimum: 3, maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }, length: { minimum: 5, maximum: 50 }
+  validates :phone, numericality: { only_integer: true }, length: { is: 11 }
+  validates :position, length: { maximum: 50 }
+  validates :salary, numericality: true
+  validates :department, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_201808) do
+ActiveRecord::Schema.define(version: 2021_03_23_225207) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_23_201808) do
     t.string "email"
     t.integer "phone"
     t.string "position"
-    t.decimal "salary"
+    t.float "salary"
     t.string "department"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
